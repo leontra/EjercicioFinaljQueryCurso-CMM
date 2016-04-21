66a9b2a536a2e041f1122cf8f94b3a05f34ac32c
jcpachuqueno <imac08@iMac-de-Imac08.local>
2016-04-20 19:14:53 -0500

    No lo logre

---
+++ js/pluginimg.js
@@ -0,0 +1,67 @@
+$.simpleLightbox({
+
+// shows fullscreen overlay
+overlay:    true,
+
+// shows loading spinner
+spinner:    true,
+
+// shows navigation arrows
+nav:      true,
+
+// text for navigation arrows
+navText:    ['&larr;','&rarr;'],
+
+// shows image captions
+captions:   true,
+captionSelector:  'img',
+captionType:    'attr',
+captionPosition:  'bottom',
+
+// captions attribute (title or data-title)
+captionsData: 'title',
+
+// shows close button
+close:      true,
+
+// text for close button
+closeText:    'X',
+
+// show counter
+showCounter:  true,
+
+// file extensions
+fileExt:    'png|jpg|jpeg|gif',
+
+// animation speed in ms
+animationSpeed: 250,
+
+// image preloading
+preloading:   true,
+
+// keyboard navigation
+enableKeyboard: true,
+
+// endless looping
+loop:     true,
+
+// closes the lightbox when clicking outside
+docClose:     true,
+
+// how much pixel you have to swipe
+swipeTolerance: 50,
+
+// lightbox wrapper Class
+className:    'simple-lightbox',
+
+// width / height ratios
+widthRatio:   0.8,
+heightRatio:  0.9,
+
+// disable right click
+disableRightClick:  false,
+
+// disable page scroll
+//disable<a href="http://www.jqueryscript.net/tags.php?/Scroll/">Scroll</a>:    true
+
+});
