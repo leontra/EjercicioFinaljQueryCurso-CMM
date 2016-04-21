d2be89dd07bf0c17702c0c8fcc8f4abaf41ed232
JulioLagarto <imac18@iMac18.local>
2016-04-20 19:23:32 -0500

    no se...

--- js/app.js
+++ js/app.js
@@ -2,4 +2,14 @@ $(document).ready(function()
 {
 
          $('#headerContainer').css('background' , '#DCDFE2');
+
+var gallery = $('.gallery a').simpleLightbox();
+
+    
+    
+    $(".fondo-full").hover(function(){
+        
+        (color("blue"));
+    });
+
 });
